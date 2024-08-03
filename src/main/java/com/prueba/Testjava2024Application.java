2a8086171323672f8442f4035d1f480ad03bf27a
Diego <diegodejuan2@gmail.com>
2024-08-03 16:56:47 +0200

    Commit inicial

---
+++ src/main/java/com/prueba/Testjava2024Application.java
@@ -0,0 +1,13 @@
+package com.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testjava2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testjava2024Application.class, args);
+	}
+
+}
